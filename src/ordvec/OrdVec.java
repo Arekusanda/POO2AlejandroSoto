2c364adf218cce8a0a6920e0c5289a3e626adf21
Administrador <Administrador@lds15.unac.edu.co>
2014-03-19 17:27:33 -0500

    Saving Changes in the Repository

---
+++ src/ordvec/OrdVec.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ordvec;
+
+/**
+ *
+ * @author Administrador
+ */
+public class OrdVec {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOrdVec jOrdVec = new JOrdVec();
+        jOrdVec.setVisible(true);
+    }
+    
+}
